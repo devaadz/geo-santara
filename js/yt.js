fc90aecc82aad8b477538de83b02cd738c904524
devaadz <devawaow@gmail.com>
2025-05-06 16:09:37 +0700

    17 test mobile

--- js/yt.js
+++ js/yt.js
@@ -1,15 +1,15 @@
 function playVideo(event) {
-    event.preventDefault(); // Biar nggak reload halaman karena klik <a>
+  event.preventDefault(); // Menghentikan perilaku default link
 
-    const container = event.currentTarget.closest(".col-md-6");
-    const thumbnail = container.querySelector("#thumbnail");
-    const videoContainer = container.querySelector("#videoContainer");
-    const videoPlayer = container.querySelector("#videoPlayer");
+  const container = event.currentTarget.closest(".col-md-6");
+  const thumbnail = container.querySelector("#thumbnail");
+  const videoContainer = container.querySelector("#videoContainer");
+  const videoPlayer = container.querySelector("#videoPlayer");
 
-    thumbnail.style.display = "none";
-    event.currentTarget.style.display = "none"; // sembunyikan tombol play
-    videoContainer.style.display = "block";
-    
-    // Ganti dengan link video yang kamu inginkan
-    videoPlayer.src = "https://www.youtube.com/embed/a3R2GO-9wTc?autoplay=1";
-  }
+  thumbnail.style.display = "none"; // Sembunyikan thumbnail
+  event.currentTarget.style.display = "none"; // Sembunyikan tombol play
+  videoContainer.style.display = "block"; // Tampilkan video container
+
+  // Ganti dengan link video YouTube yang diinginkan
+  videoPlayer.src = "https://www.youtube.com/embed/a3R2GO-9wTc?autoplay=1";
+}
